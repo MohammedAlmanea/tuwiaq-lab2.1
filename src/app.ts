3c2bac248b21caadfa941948437ab5dccceddec4
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-01-25 17:25:02 +0300

    Added app.use(express.json()) And created post method to add to the users array

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,27 @@
 import express, { Request, Response } from 'express';
+import User from './types/user.type';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+app.use(express.json());
+
 const port = 5500;
 
-type User = {
-  name: string;
-  password: string;
-};
-
-let user1: User = { name: 'Mohammed', password: '123' };
-let user2: User = { name: 'Abdullah', password: '321' };
-
-let users: User[] = [user1, user2];
-
+let users: User[] = [];
 
 app.get('/users', (req: Request, res: Response) => {
+  res.send(users);
+});
+
+app.post('/users', (req: Request, res: Response) => {
+  const newUser = req.body as User;
+  newUser.id = uuidv4();
+
+  users.push({
+    id: newUser.id,
+    name: newUser.name,
+    password: newUser.password,
+  });
   res.send(users);
 });
 
